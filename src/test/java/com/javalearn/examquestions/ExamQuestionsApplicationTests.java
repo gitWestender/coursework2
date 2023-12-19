0ee7275402230174a7ca905bdb41c99746a41706
Westender <javaxwestender@gmail.com>
2023-12-19 21:14:17 +0300

    initial

---
+++ src/test/java/com/javalearn/examquestions/ExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearn.examquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
